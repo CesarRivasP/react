94026cbc3360a65b4cb2912f57563a3b56d6e157
CesarRivasP <cesarjose58@gmail.com>
2018-12-17 16:09:48 -0400

    Settled down production webpack config

--- platzi/platzi-video/webpack.config.js
+++ platzi/platzi-video/webpack.config.js
@@ -1,61 +1,87 @@
-const path= require('path');
-const cleanWebpackPlugin= require('clean-webpack-plugin');
-const miniCssExtractPlugin= require('mini-css-extract-plugin');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = (env) => {
-  const plugins = [
-    new miniCssExtractPlugin("css/[name].[hash].css")
-  ]
-
-  if(env.NODE_ENV === 'production'){
-    plugins.push(new cleanWebpackPlugin(['dist'], {root: __dirname}))
-  }
-
-  return {
-    entry: {
-      "platzi-video": path.resolve(__dirname, 'src/index,js'),
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'js/[name]/[hash].js',
-      publicPath: path.resolve(__dirname, 'dist') + "/",
-      chunkFilename: 'js/[id].[chunkhash].js',
-    },
-    devServer: {
-      port: 9000,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env','@babel/preset-react'],
-              plugins: ['@babel/plugin-transform-runtime']
-            }
-          }
-        },
-        {
-          test: /\.css/,
-          use: [
-            miniCssExtractPlugin.loader,
-            "css-loader"
-          ]
-        },
-        {
-          test: /\.(jpg|png|gif|svg)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              fallback: 'file-loader'
-            }
+const config = {
+  mode: 'production',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[hash].css",
+      chunkFilename: "css/[id].[chunkhash].css"
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin({})]
+  },
+  entry: {
+    "platzi-video": path.resolve(__dirname, 'src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[hash].js',
+    publicPath: path.resolve(__dirname, 'dist') + "/",
+    chunkFilename: 'js/[id].[chunkhash].js'
+  },
+  devServer: {
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env', '@babel/preset-react'
+            ],
+            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread']
           }
         }
-      ]
-    },
-    plugins
+      },
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            fallback: 'file-loader',
+            name: 'img/[name].[hash].[ext]'
+          }
+        }
+      }
+    ]
   }
+}
+
+module.exports = (env, argv) => {
+	if (argv.mode === 'production') {
+		config.plugins.push(
+			new CleanWebpackPlugin(['dist'], {root:__dirname})
+		)
+	}
+	return config
 }
