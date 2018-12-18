e2f4b3808c57ab351468cb61d0e77ba9b52e6df5
CesarRivasP <cesarjose58@gmail.com>
2018-12-17 20:36:54 -0400

    Added commentaries about render of the app

--- platzi/platzi-video/src/index.js
+++ platzi/platzi-video/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = document.getElementById('app');
+// const hello = <h1>hello world</h1>;
+
+ReactDOM.render(<App />, app);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-console.log("hola mundo")
